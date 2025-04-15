0a561b640b905969a3275f76682fcc0df8247e5e
Hanre Delport <hanredelport@gmail.com>
2025-04-15 13:56:12 +0200

    Adding CSP

--- myApp.js
+++ myApp.js
@@ -12,7 +12,13 @@ app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy( 
-  {"directives": {defaultSrc : ["'self'"], scriptSrc : ["'self'", 'trusted-cdn.com']}}));
+  {"directives":
+    {
+      defaultSrc : ["'self'"], 
+      scriptSrc : ["'self'", 'trusted-cdn.com']
+    }
+  }
+));
 
 
 
